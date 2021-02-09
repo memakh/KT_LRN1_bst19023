ea2d0e427252388207df59d2fef038c5b82c5fd3
memakh <misha.mahmutov@gmail.com>
2021-02-09 10:57:50 +0300

    Second commit, added symbolCount method

--- src/LRN1_Makhmutov.java
+++ src/LRN1_Makhmutov.java
@@ -1,6 +1,14 @@
 public class LRN1_Makhmutov {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+	public static int symbolCount(String msg, char symbol) {
+		int count = 0;
+		for (char c : msg.toCharArray()) {
+			if (c == symbol) count++;
+		}
+		return count;
 	}
-
+	public static void main(String[] args) {
+		String msg = "Hello World!";
+		System.out.println(msg);
+		System.out.println(symbolCount(msg, 'l'));
+	}
 }
